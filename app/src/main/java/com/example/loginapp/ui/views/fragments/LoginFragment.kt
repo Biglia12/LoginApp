088f3454dbb7515f3abfb1eac82938c5f4a2c019
Ezequiel Biglia <ezequielbiglia12@gmail.com>
2023-12-13 21:04:09 -0300

    mejorando diseño de login y acomodando codigo

--- app/src/main/java/com/example/loginapp/ui/views/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/loginapp/ui/views/fragments/LoginFragment.kt
@@ -1,6 +1,8 @@
 package com.example.loginapp.ui.views.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -16,7 +18,7 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class LoginFragment : Fragment() {
 
-    private lateinit var  binding: FragmentLoginBinding
+    private lateinit var binding: FragmentLoginBinding
     private val loginViewModel by viewModel<LoginViewModel>()
 
 
@@ -31,49 +33,108 @@ class LoginFragment : Fragment() {
     ): View {
         binding = FragmentLoginBinding.inflate(inflater)
 
+        listenerText()
+
+        binding.buttonLogin.setOnClickListener {
+            val user: String = binding.edittextUser.text.toString()
+            val pass: String = binding.edittextPass.text.toString()
+
+            if (user.isNotEmpty() && pass.isNotEmpty()) {
+                loginViewModel.callServiceLogin(user, pass)
+            } else {
+                showHelper(user, pass)
+                requireContext().toast("Campos vacios")
+            }
+
+        }
+
         binding.textRegister.setOnClickListener {
             findNavController().navigate(R.id.registerFragment)
         }
 
-       binding.buttonLogin.setOnClickListener {
-
-           val user: String = binding.editTextUser.text.toString()
-           val pass: String = binding.editTextPass.text.toString()
-
-           if (user.isNotEmpty() || pass.isNotEmpty()){
-               loginViewModel.callServiceLogin(user, pass)
-           }else{
-               requireContext().toast("Campos vacios")
-           }
-
-       }
 
         obserVer()
 
         return binding.getRoot()
     }
 
-    fun obserVer(){
+    private fun listenerText() {
+        binding.edittextUser.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
 
-        loginViewModel.progressBar.observe(viewLifecycleOwner){
-            binding.loaderContainer.isVisible = it
-        }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
 
-        loginViewModel.errorService.observe(viewLifecycleOwner){
-            context?.toast(resources.getString(R.string.error_service))
-        }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Verifica si el texto está vacío y actualiza el HelperText en consecuencia
+                binding.titleUser.isHelperTextEnabled = s.isNullOrEmpty()
+                if (s.isNullOrEmpty()) {
+                    binding.titleUser.helperText = "Campo requerido"
+                }
+            }
+        })
 
-        // Observa la propiedad userLogued
-        loginViewModel.userLogued.observe(viewLifecycleOwner, Observer { userLogued ->
-            // Navega a homeFragment si el usuario ha iniciado sesión
-            if (userLogued) {
-                findNavController().navigate(R.id.homeFragment)
-            } else{
-                context?.toast(resources.getString(R.string.error_user))
+        binding.edittextPass.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Verifica si el texto está vacío y actualiza el HelperText en consecuencia
+                binding.pasTittle.isHelperTextEnabled = s.isNullOrEmpty()
+                if (s.isNullOrEmpty()) {
+                    binding.pasTittle.helperText = "Campo requerido"
+                }
             }
         })
 
 
+    }
+
+    private fun showHelper(user: String, pass: String) {
+
+        if (user.isEmpty()) {
+            binding.titleUser.helperText = "Campo requerido"
+        } else {
+            binding.pasTittle.isHelperTextEnabled = false
+        }
+
+        if (pass.isEmpty()) {
+            binding.pasTittle.helperText = "Campo requerido"
+        } else {
+            binding.pasTittle.isHelperTextEnabled = false
+        }
+    }
+
+    fun obserVer() {
+        observeProgressBar()
+        observeErrorService()
+        observeUserLogued()
+    }
+
+    private fun observeProgressBar() {
+        loginViewModel.progressBar.observe(viewLifecycleOwner) {
+            binding.loaderContainer.isVisible = it
+        }
+    }
+
+    private fun observeErrorService() {
+        loginViewModel.errorService.observe(viewLifecycleOwner) {
+            context?.toast(resources.getString(R.string.error_service))
+        }
+    }
+
+    private fun observeUserLogued() {
+        loginViewModel.userLogued.observe(viewLifecycleOwner) { userLogued ->
+            if (userLogued) {
+                findNavController().navigate(R.id.homeFragment)
+            } else {
+                context?.toast(resources.getString(R.string.error_user))
+            }
+        }
     }
 
 }
